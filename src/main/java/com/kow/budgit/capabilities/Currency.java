d353542c44d8c9af4c9f9528b71695e8e1faf26e
KindOfWay <admin@sysbite.org>
2020-03-04 08:13:58 +0200

    Added default value for initial capability value

--- src/main/java/com/kow/budgit/capabilities/Currency.java
+++ src/main/java/com/kow/budgit/capabilities/Currency.java
@@ -1,7 +1,7 @@
 package com.kow.budgit.capabilities;
 
 public class Currency implements ICurrency {
-	private Integer amount;
+	private Integer amount = 0;
 
 	@Override
 	public void set(int rhs) {
